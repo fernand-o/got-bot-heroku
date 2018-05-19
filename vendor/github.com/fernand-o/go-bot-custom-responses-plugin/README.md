79e401a0b150be539a90c6344a7b60296d9074b2
Fernando Almeida <fernandoprsbr@gmail.com>
2018-05-19 01:30:57 -0300

    Update dependencies

---
+++ vendor/github.com/fernand-o/go-bot-custom-responses-plugin/README.md
@@ -0,0 +1,13 @@
+A plugin for [go-bot](https://github.com/go-chat-bot/bot) that allows defining custom responses with given patterns
+
+- [x] Create project basics
+- [x] Define methods structure
+- [x] Create some tests
+- [x] Connect with redis
+- [x] Create command to set patterns/responses
+- [x] Apply regex to find responses from patterns
+- [x] Create and configure heroku redis app
+- [ ] Deploy a bot instance and test with slack
+- [ ] Create command to list defined responses
+- [ ] Create command to delete/clear defined responses
+- [ ] Allow defining prefixes for conditions (to avoid processing all received messages)
---
+++ vendor/github.com/fernand-o/go-bot-custom-responses-plugin/customresponses.go
@@ -0,0 +1,91 @@
+package customresponses
+
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/go-chat-bot/bot"
+	"github.com/go-redis/redis"
+)
+
+const (
+	argumentsExample = "!setReponse 'Found a banana' 'banana*'"
+	invalidArguments = "Please inform the params, ex:"
+)
+
+var Keys []string
+
+func newClient() *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+}
+
+func loadMessages() {
+	var err error
+	client := newClient()
+	Keys, err = client.Keys("*").Result()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func setResponse(pattern string, response string) {
+	client := newClient()
+	err := client.Set(pattern, response, 0).Err()
+	if err != nil {
+		panic(err)
+	}
+	loadMessages()
+}
+
+func getResponse(pattern string) string {
+	client := newClient()
+	response, err := client.Get(pattern).Result()
+	if err != nil {
+		panic(err)
+	}
+	return response
+}
+
+func responseMessage(response, pattern string) string {
+	return fmt.Sprintf("Ok! I will send a message with %s when i found any matches with %s", response, pattern)
+}
+
+func setReponseCommand(command *bot.Cmd) (msg string, err error) {
+	if len(command.Args) != 2 {
+		msg = argumentsExample
+		return
+	}
+	response := command.Args[0]
+	pattern := command.Args[1]
+	setResponse(pattern, response)
+	msg = responseMessage(pattern, response)
+	return
+}
+
+func customresponses(command *bot.PassiveCmd) (msg string, err error) {
+	var match bool
+	for _, k := range Keys {
+		match, err = regexp.MatchString(k, command.Raw)
+		if match {
+			msg = getResponse(k)
+			break
+		}
+	}
+	return
+}
+
+func init() {
+	bot.RegisterPassiveCommand(
+		"customresponses",
+		customresponses)
+	bot.RegisterCommand(
+		"setResponse",
+		"Defines a custom response for the given pattern",
+		argumentsExample,
+		setReponseCommand)
+	loadMessages()
+}
